599c7d9d0b1228f9bc0eaadd0e8a090047cd15be
Yauheni Karaniuk <ykaraniuk@fixnetwork.com>
2025-02-28 14:02:45 -0500

    Added 10 dice components. Added button design for the game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import Die from "./Die"
+
 function App() {
 
   return (
     <>
-      <main className="game-board"></main>
+      <main className="game-board">
+        <div className="dice-container">
+          <Die value={1} />
+          <Die value={2} />
+          <Die value={3} />
+          <Die value={4} />
+          <Die value={5} />
+          <Die value={5} />
+          <Die value={4} />
+          <Die value={3} />
+          <Die value={2} />
+          <Die value={1} />
+        </div>
+      </main>
     </>
   )
 }
